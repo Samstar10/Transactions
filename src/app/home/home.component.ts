8a9ea8b9517f1d334e4d264edfdb415b559746da
Samuel Muli <samuelmuli95@gmail.com>
2024-01-21 12:52:47 +0300

    Initiate transaction list interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { TransactionTableComponent } from '../transaction-table/transaction-table.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TransactionTableComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
