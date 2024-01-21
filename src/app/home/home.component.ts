c5813682f61199a90452c0f6b4acbb249535b56f
Samuel Muli <samuelmuli95@gmail.com>
2024-01-21 13:56:07 +0300

    Inject the new service into home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TransactionTableComponent } from '../transaction-table/transaction-table.component';
 import { CommonModule } from '@angular/common';
 import { Transactionslist } from '../transactionslist';
+import { TransactionsService } from '../transactions.service';
 
 @Component({
   selector: 'app-home',
@@ -14,34 +15,10 @@ import { Transactionslist } from '../transactionslist';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  transactions: Transactionslist[] = [
-    {
-      id: 1,
-      date: '2022-01-01',
-      description: 'Transaction 1',
-      category: 'Category 1',
-      amount: 100
-    },
-    {
-      id: 2,
-      date: '2022-01-02',
-      description: 'Transaction 2',
-      category: 'Category 2',
-      amount: 200
-    },
-    {
-      id: 3,
-      date: '2022-01-03',
-      description: 'Transaction 3',
-      category: 'Category 3',
-      amount: 300
-    },
-    {
-      id: 4,
-      date: '2022-01-04',
-      description: 'Transaction 4',
-      category: 'Category 4',
-      amount: 400
-    }
-  ]
+  transactions: Transactionslist[] = [];
+  housingService: TransactionsService = inject(TransactionsService);
+
+  constructor() {
+    this.transactions = this.housingService.getAllTransactions();
+  }
 }
