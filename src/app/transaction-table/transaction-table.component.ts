7ae3e5752b6f1168aae71beca6b6a074a4dd335e
Samuel Muli <samuelmuli95@gmail.com>
2024-01-21 13:10:27 +0300

    Add the input property

--- src/app/transaction-table/transaction-table.component.ts
+++ src/app/transaction-table/transaction-table.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Transactionslist } from '../transactionslist';
 
 @Component({
   selector: 'app-transaction-table',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './transaction-table.component.html',
   styleUrl: './transaction-table.component.css'
 })
 export class TransactionTableComponent {
-
+  @Input() transactions!: Transactionslist;
 }
