0edd7c9d32d51ec8bcc856045dd0c2f0cee23c9b
Samuel Muli <samuelmuli95@gmail.com>
2024-01-22 15:38:19 +0300

    Add Transaction Form Component

---
+++ src/app/add-transaction-form/add-transaction-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-transaction-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-transaction-form.component.html',
+  styleUrl: './add-transaction-form.component.css'
+})
+export class AddTransactionFormComponent {
+
+}
